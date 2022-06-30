217eb627da8830587b15c6828dbbd60badbffd63
xurich-xulaco <82631735+xurich-xulaco@users.noreply.github.com>
2022-06-30 11:39:32 -0500

    Including non-free stuff

---
+++ Auto-fusion.sh
@@ -0,0 +1,11 @@
+#!/bin/env bash
+##### CHECK FOR SUDO or ROOT ##################################
+if ! [ $(id -u) = 0 ]; then
+  echo "This script must be run as sudo or root, try again..."
+  exit 1
+fi
+
+dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+  https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+  
+#Just for the sake of having as much convinience as posible within the first boot
