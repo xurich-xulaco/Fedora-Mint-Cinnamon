7326cfaa493cda88297f4167372346578d808a8c
xurich-xulaco <82631735+xurich-xulaco@users.noreply.github.com>
2022-06-30 13:53:28 -0500

    Some touch up

--- Auto-intelhw.sh
+++ Auto-intelhw.sh
@@ -5,11 +5,12 @@ if ! [ $(id -u) = 0 ]; then
   exit 1
 fi
 
-dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
+dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin #Multimedia is was already installed, upgrading...
 dnf install libva-intel-hybrid-driver \ #This just depends upon your hardware, maybe this script will be more frindly in the future
   libva \
   libva-utils \
   gstreamer1-vaapi \
   ffmpeg \
   intel-gpu-tools \
-  mesa-dri-drivers mpv
+  mesa-dri-drivers \
+  mpv
